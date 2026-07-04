b9e1fb6a50431c64fe2f5dfad83bb53909147089
vishvapatel86 <pvishva673@gmail.com>
2026-07-04 21:16:07 +0530

    Completed Day 1 - Hello World

---
+++ day_1/helloworld.py
@@ -0,0 +1,29 @@
+print("hello world")
+print("addition:", 3 + 4)
+print("subtraction:", 3 - 4)
+print("multiplication:", 3 * 4)
+print("modulus:", 3 % 4)
+print("division:", 3 / 4)
+print("exponential", 3 ** 4)
+print("floor divison:", 3 // 4)
+print("vishva")
+print("patel")
+print("india")
+print("I am enjoying 30 days of python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['asabeneh', 'python', 'finland']))
+print(type('apple'))
+print(type({1,2,3,4,5}))
+print(type((1,2,3,4,5)))
+print(type({'name': 'vishva', 'age': 20, 'country': 'india'}))
+print(type(True))
+import math
+x1 , y1 = 2 ,3
+x2 , y2 = 10 , 8
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print("Euclidean distance :", distance)
+
+
