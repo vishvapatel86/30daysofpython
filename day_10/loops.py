36a2b2b303a16c37eeffbc0047a381fb50aa127c
vishvapatel86 <pvishva673@gmail.com>
2026-07-21 22:36:19 +0530

    day_10 : completed exercise 1

--- day_10/loops.py
+++ day_10/loops.py
@@ -21,3 +21,32 @@ while i >= 0:
 #Seven Calls to print() to Draw a Triangle
 for i in range(1, 8):
     print("#" * i)
+
+# Creates a 6x6 grid of '#' characters
+for i in range(6):
+    for j in range(6):
+        print("#", end=" ")
+    print()
+
+# Iterates from 0 to 10 to print formatted multiplication equations
+for i in range(11):
+    print(f"{i} x {i} = {i * i}")
+
+#Iterate through a list
+tech_list = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+
+for item in tech_list:
+    print(item)
+
+#Print only even numbers (0 to 100)
+
+for i in range(101):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+#Print only odd numbers (0 to 100)
+for i in range(101):
+    if i % 2 != 0:
+        print(i, end=" ")
+  
+  
